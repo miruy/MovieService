daa0302bbf401e0255c492ad8986864fbbe2e3a3
miruy <dbflarla4966@naver.com>
2022-11-10 19:48:47 +0900

    update Movie Service_Class Component ver.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    loading: true,
+    movies: []
+  }
+
+  getMovies = async () => {
+    // ↓movies : axios - API의 movies
+    const { data: { data: { movies } } } = await (axios.get("https://yts.mx/api/v2/list_movies.json"));
+
+    // ↓movies : setState()의 movies
+    this.setState({ movies, loading: false });
+
+    // * axios - API의 movies를 setState()를 사용하여 state의 movies로 저장
+  }
+
+  // 보통 API를 componentDidMount()안에 fetch
+  componentDidMount() {
+    this.getMovies();
+  }
+
+  render() {
+    const { loading } = this.state;
+    return (
+      <div>{loading ? "Loading..." : "We Are Ready!"}</div>
+    );
+  }
 }
 
 export default App;
